7a4a25bb72b403b7cc84c4fac7330ac1334b8166
Hoang Nguyen Duy <hoangnd689x@gmail.com>
2021-09-28 12:27:40 +0200

    Updates

---
+++ precache-manifest.a54a2b59edf04f21fa6e3e0bda7f4a13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a72a66b2f1cce2244208",
+    "url": "/me/static/css/main.0504ed59.chunk.css"
+  },
+  {
+    "revision": "a72a66b2f1cce2244208",
+    "url": "/me/static/js/main.a72a66b2.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "1b27b5c61a4328e41ab14c9ec3cb382a",
+    "url": "/me/index.html"
+  }
+];
